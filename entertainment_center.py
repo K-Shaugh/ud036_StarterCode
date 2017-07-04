25f4febb36c01f2ef0eb1c41fc3804ca4b399a81
Dinzer <shaugh77@gmail.com>
2017-07-04 10:32:53 -0700

    Movie Trailer Website - Resubmission

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,8 @@
 import fresh_tomatoes
 import media
 
-#Creates instances of media.Movie with title, movie poster url, and
-#youtube url attributes
+# Creates instances of media.Movie with title, movie poster url, and
+# youtube url attributes
 
 napolean_dynamite = media.Movie("Napolean Dynamite",
                                                 "https://upload.wikimedia.org/wikipedia/en/8/87/Napoleon_dynamite_post.jpg",  # NOQA
@@ -14,7 +14,7 @@ tropic_thunder = media.Movie("Tropic Thunder",
 
 high_noon = media.Movie("High Noon",
                                        "https://upload.wikimedia.org/wikipedia/en/5/54/High_Noon_poster.jpg",  # NOQA
-                                       "https://youtu.be/Gh-vOc-gwZs")                     
+                                       "https://youtu.be/Gh-vOc-gwZs")
 
 fight_club = media.Movie("Fight Club",
                                       "https://vignette1.wikia.nocookie.net/fightclub/images/6/6a/Fight-club-dvd.jpg/revision/latest?cb=20081116042426",  # NOQA
@@ -26,7 +26,7 @@ new_hope = media.Movie("Star Wars: A New Hope",
 
 matrix = media.Movie("The Matrix",
                                  "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # NOQA
-                                 "https://youtu.be/vKQi3bBA1y8") 
+                                 "https://youtu.be/vKQi3bBA1y8")
 
 pick_of_destiny = media.Movie("Pick of Destiny",
                                               "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Tenacious_d_in_the_pick_of_destiny_ver3.jpg/220px-Tenacious_d_in_the_pick_of_destiny_ver3.jpg",  # NOQA
@@ -34,18 +34,17 @@ pick_of_destiny = media.Movie("Pick of Destiny",
 
 dodgeball = media.Movie("Dodgeball",
                                       "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Movie_poster_Dodgeball_A_True_Underdog_Story.jpg/220px-Movie_poster_Dodgeball_A_True_Underdog_Story.jpg",  # NOQA
-                                      "https://youtu.be/W-XbDZUnUmw") 
+                                      "https://youtu.be/W-XbDZUnUmw")
 
 good_will_hunting = media.Movie("Good Will Hunting",
                                                 "https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Good_Will_Hunting_theatrical_poster.jpg/220px-Good_Will_Hunting_theatrical_poster.jpg",  # NOQA
                                                 "https://youtu.be/nH9LZOXBMUE")
 
-#open_movies_page function creates HTML page displaying
-#the movies from the movies list
+# open_movies_page function creates HTML page displaying
+# the movies from the movies list
 
 movies = [napolean_dynamite, tropic_thunder, high_noon,
-                fight_club, new_hope, matrix, pick_of_destiny,
-                dodgeball, good_will_hunting]
+          fight_club, new_hope, matrix, pick_of_destiny,
+          dodgeball, good_will_hunting]
 
-fresh_tomatoes.open_movies_page(movies)    
-
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 import webbrowser
 
+
 class Movie():
 
     def __init__(self, movie_title, poster_image, trailer_youtube):
-        #Initializes Movie class with title, poster image, and youtube trailer
+        # Initializes Movie class with title, poster image, and youtube trailer
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        #defines function to open the youtube trailer
+        # defines function to open the youtube trailer
         webbrowser.open(self.trailer_youtube_url)
